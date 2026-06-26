f13fb74b4811ccc7f012b4a435e1579c45c66fde
raynaro <raynarobmx133@gmail.com>
2026-06-25 21:54:42 -0500

    Add files via upload

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Raynaro\Desktop\idat-sisten\v13\venv"
+set "VIRTUAL_ENV=C:\Users\USER\Desktop\Nueva carpeta (3)\FINAL PROYECT\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- Scripts/activate.fish
+++ Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Raynaro\Desktop\idat-sisten\v13\venv'
+set -gx VIRTUAL_ENV 'C:\Users\USER\Desktop\Nueva carpeta (3)\FINAL PROYECT\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
